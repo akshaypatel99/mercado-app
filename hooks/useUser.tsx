4aed408882787f9791fcb7e5ff8f6c811dc3be15
akshaypatel99 <akshaypatel99@gmail.com>
2022-02-10 22:38:55 +0000

    Add current user hook to client

---
+++ hooks/useUser.tsx
@@ -0,0 +1,51 @@
+import { gql, useQuery } from '@apollo/client';
+
+const CURRENT_USER = gql`
+	query CurrentUser {
+		currentUser {
+			_id
+			name
+			email
+			role
+			userProducts {
+				_id
+				name
+				description
+				image
+				category
+				price
+			}
+			userOrders {
+				_id
+				orderItems {
+					_id
+				}
+				orderTotal
+				deliveryCost
+				totalCost
+				deliveryAddress {
+					street
+					city
+					postcode
+				}
+				deliveryDate
+				paymentResult {
+					id
+					status
+					updatedAt
+					emailAddress
+				}
+				isPaid
+				paidAt
+				createdAt
+			}
+		}
+	}
+`;
+
+export function useUser() {
+	const { data, loading, error } = useQuery(CURRENT_USER);
+	return { data, loading, error };
+}
+
+export { CURRENT_USER };
