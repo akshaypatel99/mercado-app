89f7d340dedc27d1515a2d1637c46778286e0a48
akshaypatel99 <akshaypatel99@gmail.com>
2022-02-11 12:55:45 +0000

    Add current user data to AuthContext

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../context/auth';
 
 const CURRENT_USER = gql`
 	query CurrentUser {
@@ -45,6 +47,14 @@ const CURRENT_USER = gql`
 
 export function useUser() {
 	const { data, loading, error } = useQuery(CURRENT_USER);
+	const auth = useContext(AuthContext);
+
+	useEffect(() => {
+		if (data) {
+			auth.setUser(data.currentUser);
+		}
+	}, [data, auth]);
+
 	return { data, loading, error };
 }
 
