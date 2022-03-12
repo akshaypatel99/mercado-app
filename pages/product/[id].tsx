61ab188b62ac8f7b109dc901e34d2f60d92b61d7
akshaypatel99 <akshaypatel99@gmail.com>
2022-03-12 00:45:45 +0000

    Add SSR to single product page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,14 +1,50 @@
-import { useRouter } from 'next/router';
+import { gql } from '@apollo/client';
+import client from '../../lib/apollo-client';
 import Product from '../../components/Product';
+import checkUser, { MyPageContext } from '../../lib/checkUser';
+import { GetServerSideProps } from 'next';
 
-export default function ProductPage() {
-	const router = useRouter();
-	const { id } = router.query;
-
-	let prodId = '';
-	if (id && typeof id === 'string') {
-		prodId = id;
-	}
-
-	return <Product id={prodId} />;
+export default function ProductPage({ product, error, user }) {
+	return <Product product={product} error={error} user={user} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+	context: MyPageContext
+) => {
+	await checkUser(context, false);
+
+	const { id } = context.params;
+
+	const { data, error } = await client.query({
+		query: gql`
+			query SingleProduct($productId: ID!) {
+				product(id: $productId) {
+					_id
+					name
+					description
+					image
+					category
+					price
+					user {
+						_id
+						name
+					}
+					watchedBy {
+						_id
+					}
+				}
+			}
+		`,
+		variables: {
+			productId: id,
+		},
+	});
+
+	return {
+		props: {
+			product: data.product,
+			error: error || null,
+			user: context.user,
+		},
+	};
+};
