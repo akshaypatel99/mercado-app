639a89e5d85b5691c4921b71988869375cd9e0db
akshaypatel99 <akshaypatel99@gmail.com>
2022-03-16 21:41:10 +0000

    Add back link

---
+++ components/BackLink.tsx
@@ -0,0 +1,25 @@
+import { Box, Link, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import Router from 'next/router';
+import { FiArrowLeft } from 'react-icons/fi';
+
+export default function BackLink() {
+	return (
+		<>
+			<Box
+				as='button'
+				fontSize='md'
+				color='brand.500'
+				fontWeight='semibold'
+				display='inline-flex'
+				w='max-content'
+				alignItems='center'
+				_hover={{ textDecoration: 'none', color: 'brand.750' }}
+				_focus={{ boxShadow: 'none', color: 'brand.750' }}
+				onClick={() => Router.back()}
+			>
+				<FiArrowLeft /> <Text ml='1'>Back</Text>
+			</Box>
+		</>
+	);
+}
