9220dd96f3add2edf3599496d1fe0752b208992b
akshaypatel99 <akshaypatel99@gmail.com>
2022-03-17 23:47:54 +0000

    Add DeleteDialog component

---
+++ components/DeleteDialog.tsx
@@ -0,0 +1,57 @@
+import { useRef } from 'react';
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	Button,
+	Text,
+} from '@chakra-ui/react';
+
+export default function DeleteDialog({
+	isOpen,
+	onClose,
+	heading,
+	handleDelete,
+	id,
+}) {
+	const cancelRef = useRef();
+
+	return (
+		<>
+			<AlertDialog
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+							Delete {heading}
+						</AlertDialogHeader>
+
+						<AlertDialogBody fontWeight='semibold'>
+							<Text mb='2'>
+								Delete {heading} #{id}?
+							</Text>
+							<Text>
+								Are you sure? You can&apos;t undo this action afterwards.
+							</Text>
+						</AlertDialogBody>
+
+						<AlertDialogFooter>
+							<Button colorScheme='yellow' ref={cancelRef} onClick={onClose}>
+								Cancel
+							</Button>
+							<Button colorScheme='red' onClick={() => handleDelete(id)} ml={3}>
+								Delete
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</>
+	);
+}
