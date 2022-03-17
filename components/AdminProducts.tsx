7c8f6ee9fc0195ba9f89db46743c85d948fc923c
akshaypatel99 <akshaypatel99@gmail.com>
2022-03-17 23:48:44 +0000

    Add delete and restock functionality

--- components/AdminProducts.tsx
+++ components/AdminProducts.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import {
 	Table,
 	Thead,
 	Tbody,
+	Tfoot,
 	Tr,
 	Th,
 	Td,
@@ -12,16 +14,97 @@ import {
 	LinkOverlay,
 	IconButton,
 	Text,
+	useDisclosure,
 } from '@chakra-ui/react';
+import { gql, useMutation } from '@apollo/client';
 import NextLink from 'next/link';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { localDate } from '../lib/localDate';
-import { FiCheck, FiInfo, FiRotateCcw, FiTrash2, FiX } from 'react-icons/fi';
+import { FiInfo, FiMinus, FiRotateCcw, FiTrash2 } from 'react-icons/fi';
+import formatCurrency from '../lib/formatCurrency';
+import DeleteDialog from './DeleteDialog';
+import ErrorMessage from './ErrorMessage';
+import InfoMessage from './InfoMessage';
+
+const DELETE_PRODUCT = gql`
+	mutation DeleteProduct($deleteProductId: ID!) {
+		deleteProduct(id: $deleteProductId) {
+			message
+			product {
+				_id
+			}
+		}
+	}
+`;
+
+const RESTOCK_PRODUCT = gql`
+	mutation Restock($restockProductId: ID!) {
+		restock(id: $restockProductId) {
+			message
+			product {
+				_id
+			}
+		}
+	}
+`;
 
 export default function AdminProducts({ products, count }) {
+	const [productToDelete, setProductToDelete] = useState(null);
+	const [error, setError] = useState(null);
+	const router = useRouter();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const [deleteProduct, { data: deleteData, error: deleteError }] = useMutation(
+		DELETE_PRODUCT,
+		{
+			onCompleted: () =>
+				Router.push(`/admin/products?message=Product deleted!`),
+		}
+	);
+
+	const [restockProduct, { data: restockData, error: restockError }] =
+		useMutation(RESTOCK_PRODUCT, {
+			onCompleted: () =>
+				Router.push(`/admin/products?message=Product restocked!`),
+		});
+
+	const handleDelete = (productId) => {
+		deleteProduct({
+			variables: {
+				deleteProductId: productId,
+			},
+		});
+		if (deleteError) {
+			setError(deleteError);
+		}
+		setProductToDelete(null);
+		onClose();
+	};
+
+	const handleRestock = (productId) => {
+		restockProduct({
+			variables: {
+				restockProductId: productId,
+			},
+		});
+		if (restockError) {
+			setError(restockError);
+		}
+	};
+
+	function returnId(product) {
+		const order = product.orders.find(
+			(order) => order.createdAt.slice(0, 19) === product.soldOn.slice(0, 19)
+		);
+		return order ? order._id : null;
+	}
+
 	return (
 		<>
-			<Text fontWeight='semibold'>Total: {count}</Text>
+			{error && <ErrorMessage error={error} />}
+			{router && router.query.message && (
+				<InfoMessage message={router.query.message} />
+			)}
 			<Table variant='simple' color='brand.700'>
 				<TableCaption placement='top'>
 					Click the information icon to see further details
@@ -32,7 +115,7 @@ export default function AdminProducts({ products, count }) {
 						<Th>Name</Th>
 						<Th>Created</Th>
 						<Th isNumeric>Price (£)</Th>
-						<Th>Sold</Th>
+						<Th>Sold On</Th>
 						<Th>Restock</Th>
 						<Th>User</Th>
 						<Th>Info</Th>
@@ -66,12 +149,23 @@ export default function AdminProducts({ products, count }) {
 								</Td>
 								<Td>{localDate(product.createdAt)}</Td>
 								<Td isNumeric>{product.price.toFixed(2)}</Td>
-								<Td>{product.isSold ? <FiCheck /> : <FiX />}</Td>
+								<Td>
+									{product.soldOn ? (
+										<NextLink href={`/order/${returnId(product)}`} passHref>
+											<Link>{localDate(product.soldOn)}</Link>
+										</NextLink>
+									) : (
+										<FiMinus />
+									)}
+								</Td>
 								<Td>
 									<IconButton
 										icon={<FiRotateCcw />}
 										aria-label='Return to stock'
-										onClick={() => {}}
+										disabled={!product.isSold}
+										onClick={() => {
+											handleRestock(product._id);
+										}}
 									/>
 								</Td>
 								<Td>
@@ -91,13 +185,37 @@ export default function AdminProducts({ products, count }) {
 									<IconButton
 										icon={<FiTrash2 />}
 										aria-label='Delete product'
-										onClick={() => {}}
+										onClick={() => {
+											setProductToDelete(product._id);
+											onOpen();
+										}}
 									/>
 								</Td>
 							</Tr>
 						))}
 				</Tbody>
+				<Tfoot>
+					<Tr>
+						<Td colSpan={3}>
+							<Text fontWeight='semibold'>Total: {count}</Text>
+						</Td>
+						<Td isNumeric fontWeight='bold'>
+							{formatCurrency(
+								products.reduce((acc, product) => {
+									return acc + product.price;
+								}, 0)
+							)}
+						</Td>
+					</Tr>
+				</Tfoot>
 			</Table>
+			<DeleteDialog
+				isOpen={isOpen}
+				onClose={onClose}
+				heading='Product'
+				handleDelete={handleDelete}
+				id={productToDelete}
+			/>
 		</>
 	);
 }
